f9a41de8e34606ab041ea89d13f4e267873c6448
phvleite <phvleite@gmail.com>
2022-06-19 23:28:54 -0300

    [REQ-05 e 06] realizado

--- routerTalker.js
+++ routerTalker.js
@@ -3,9 +3,14 @@ const rescue = require('express-rescue');
 
 const router = express.Router();
 const {
-  // authMiddleware,
+  authMiddleware,
   getDataFile,
-} = require('./utils');
+  setDataFile,
+  validateTalkerName,
+  validateTalkerAge,
+  validateTalkerTalkWatchedAt,
+  validateTalkerTalkRate, 
+  validateTalkerTalk } = require('./utils');
 
 const dbTalker = 'talker';
 
@@ -27,6 +32,52 @@ router.get('/:id', rescue(async (req, res) => {
   res.status(200).json(talker);
 }));
 
+router.post('/',
+  authMiddleware,
+  validateTalkerName,
+  validateTalkerAge,
+  validateTalkerTalk,
+  validateTalkerTalkWatchedAt,
+  validateTalkerTalkRate,
+  rescue(async (req, res) => {
+  const { name, age, talk } = req.body;
+
+  const talkers = await getDataFile(dbTalker);
+  
+  const id = Number(talkers[talkers.length - 1].id) + 1;
+
+  talkers.push({ id, name, age, talk });
+
+  await setDataFile(dbTalker, talkers);
+
+  res.status(201).json({ id, name, age, talk });
+}));
+
+router.put('/:id',
+  authMiddleware,
+  validateTalkerName,
+  validateTalkerAge,
+  validateTalkerTalk,
+  validateTalkerTalkWatchedAt,
+  validateTalkerTalkRate,
+  rescue(async (req, res) => {
+  const { name, age, talk } = req.body;
+  let { id } = req.params;
+  id = Number.parseInt(id, 10);
+
+  const talkers = await getDataFile(dbTalker);
+
+  const talkerIndex = talkers.findIndex((tal) => tal.id === id);
+  
+  if (talkerIndex === -1) return res.status(400).json({ message: 'Registro não encontrado' });
+  
+  talkers[talkerIndex] = { ...talkers[talkerIndex], name, age, talk };
+
+  await setDataFile(dbTalker, talkers);
+
+  res.status(200).json({ id, name, age, talk });
+}));
+
 // router.get('/search', rescue(async (req, res) => {
 //   const { name, maxPrice, minPrice } = req.query;
 //   const drinks = await getDataFile(dbDrinks);
@@ -39,50 +90,19 @@ router.get('/:id', rescue(async (req, res) => {
 //   res.status(200).json(filteredDrinks);
 // }));
 
-// router.post('/', validateNamePrice, rescue(async (req, res) => {
-//   const { id, name, price } = req.body;
-//   const { username } = req.user;
-//   const drinks = await getDataFile(dbDrinks);
-
-//   if (drinks.some((drink) => drink.id === id)) {
-//     return res.status(409).json({ message: 'id already exist' });
-//   }
-
-//   drinks.push({ id, name, price, barman: username });
-
-//   await setDataFile(dbDrinks, drinks);
-
-//   res.status(201).json({ message: 'Drink created sucessfully!' });
-// }));
-
-// router.put('/:id', validateNamePrice, rescue(async (req, res) => {
-//   const { id } = req.params;
-//   const { name, price } = req.body;
-//   const drinks = await getDataFile(dbDrinks);
-//   const drinkIndex = drinks.findIndex((r) => r.id === Number(id));
+router.delete('/:id', authMiddleware, rescue(async (req, res) => {
+  const { id } = req.params;
+  const talkers = await getDataFile(dbTalker);
+  const talkerIndex = talkers.findIndex((tal) => tal.id === Number(id));
   
-//   if (drinkIndex === -1) return res.status(404).json({ message: 'Drink not found!' });
+  if (talkerIndex === -1) return res.status(404).json({ message: 'Registro não encontrado!' });
   
-//   drinks[drinkIndex] = { ...drinks[drinkIndex], name, price };
+  talkers.splice(talkerIndex, 1);
   
-//   await setDataFile(dbDrinks, drinks);
+  await setDataFile(dbTalker, talkers);
 
-//   res.status(204).end();
-// }));
-
-// router.delete('/:id', rescue(async (req, res) => {
-//   const { id } = req.params;
-//   const drinks = await getDataFile(dbDrinks);
-//   const drinkIndex = drinks.findIndex((r) => r.id === Number(id));
-  
-//   if (drinkIndex === -1) return res.status(404).json({ message: 'Drink not found!' });
-  
-//   drinks.splice(drinkIndex, 1);
-  
-//   await setDataFile(dbDrinks, drinks);
-
-//   res.status(204).end();
-// }));
+  res.status(204).end();
+}));
 
 router.use((err, _req, res, _next) => {
   res.status(500).json({ error: `Erro: ${err.message}` });
--- utils.js
+++ utils.js
@@ -21,13 +21,14 @@ const generateToken = () => crypto.randomBytes(8).toString('hex');
 
 const authMiddleware = (req, res, next) => {
   const { authorization } = req.headers;
-  if (!authorization || authorization.length !== 16) {
-    return res.status(401).json({ message: 'Token inválido!' });
+  if (!authorization) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (authorization.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
   }
   next();
 };
-
-const regexEmail = new RegExp('[a-z0-9]+@[a-z]+\\.[a-z]{2,3}');
 
 const validateLogin = (req, res, next) => {
   const { email, password } = req.body;
@@ -43,6 +44,8 @@ const validateLoginEmail = (req, res, next) => {
   const { email } = req.body;
 
   if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+
+  const regexEmail = new RegExp('[a-z0-9]+@[a-z]+\\.[a-z]{2,3}');
 
   if (!regexEmail.test(email)) {
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
@@ -63,6 +66,64 @@ const validateLoginPassword = (req, res, next) => {
   next();
 };
 
+const validateTalkerName = (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+
+  if (name.length < 3) { 
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  next();
+};
+
+const validateTalkerAge = (req, res, next) => {
+  const { age } = req.body;
+
+  if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+
+  if (Number(age) < 18) { 
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  next();
+};
+
+const validateTalkerTalk = (req, res, next) => {
+  const { talk } = req.body;
+
+  if (!talk) return res.status(400).json({ message: 'O campo "talk" é obrigatório' });
+
+  next();
+};
+
+const validateTalkerTalkWatchedAt = (req, res, next) => {
+  const { watchedAt } = req.body.talk;
+
+  if (!watchedAt) return res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
+
+  const regexDate = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+
+  if (!regexDate.test(watchedAt)) { 
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+
+  next();
+};
+
+const validateTalkerTalkRate = (req, res, next) => {
+  const { rate } = req.body.talk;
+
+  if (!rate && rate !== 0) return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
+
+  if (Number(rate) < 1 || Number(rate) > 5) { 
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  next();
+};
+
 module.exports = {
   consoleReq,
   getDataFile,
@@ -72,4 +133,9 @@ module.exports = {
   validateLogin,
   validateLoginEmail,
   validateLoginPassword,
+  validateTalkerAge,
+  validateTalkerName,
+  validateTalkerTalk,
+  validateTalkerTalkWatchedAt,
+  validateTalkerTalkRate,
 };
